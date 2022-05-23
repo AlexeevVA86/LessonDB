c15ca867f771c033d99aa3b73f0f8b61259c55c4
Alex86 <1a2v3a@gmail.com>
2022-05-23 15:33:11 +0500

    второе дз это боль и слёзы но 3-и задания готовы. поехали делать 4 -е.

--- Lesson02/02.py
+++ Lesson02/02.py
@@ -5,22 +5,16 @@
 # кавычками (добавить кавычку до и кавычку после элемента списка, являющегося числом) и
 # дополнить нулём до двух целочисленных разрядов:
 
-
-word_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-# for word in word_list:
-#     found_num = []
-#     for symb in word:
-#         if symb in digits:
-#             found_num.append(symb)
-#         else:
-#             if len(found_num) > 0:
-#                 print("Found number {:0>2}".format(''.join(found_num)))
-#     if len(found_num) > 0:
-#         print("\"" + "{:0>2}".format(''.join(found_num)) + "\"")
-
-my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-i = 1
-while i < len(my_list):
-    my_list.insert(i, '"')
-    i += 2
+weather = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+weather2 = []
+for i in range(len(weather)):
+    circumcision = weather.pop(0)
+    if  circumcision.isdigit():
+        weather2.append(F'"{int(circumcision):02d}"')
+    elif circumcision[0] == "-":
+        weather2.append(F'"{int(circumcision):03d}"')
+    elif circumcision[0] == "+":
+        weather2.append(F'"+{int(circumcision):02d}"')
+    else:
+        weather2.append(circumcision)
+print(' '.join(weather2))
--- Lesson02/03.py
+++ Lesson02/03.py
@@ -1,6 +1,14 @@
-a = ['В', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-b = ['{:0>2}'.format(number) for number in a]
+# 3. *(вместо задачи 2) Решить задачу 2 не создавая новый список
 
-message = ''
-for item in a:
-    print(item, end=' ')
+weather = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+for i in range(len(weather)):
+    circumcision = weather.pop(0)
+    if  circumcision.isdigit():
+        weather.append(F'"{int(circumcision):02d}"')
+    elif circumcision[0] == "-":
+        weather.append(F'"{int(circumcision):03d}"')
+    elif circumcision[0] == "+" and circumcision[1].isdigit():
+        weather.append(F'"+{int(circumcision):02d}"')
+    else:
+        weather.append(circumcision)
+print(' '.join(weather))
