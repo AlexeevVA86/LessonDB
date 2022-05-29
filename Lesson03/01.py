5a169655e24965540482eed37e9190c16d63b936
Alex86 <1a2v3a@gmail.com>
2022-05-29 13:07:15 +0500

    первое задание 3 урака

--- Lesson03/01.py
+++ Lesson03/01.py
@@ -1,10 +1,11 @@
 # 1. Написать функцию num_translate(), переводящую числительные от 0 до 10 c английского на русский язык.
 
-numbers = {'0': 'zero', '1': 'one', '3': 'tre', '4': 'four', '5': 'five', '6': 'secs', '7': 'seven', '8': 'eith', '9': 'night', '10': 'ten'}
+numbers = {0: 'zero', 1: 'one', 3: 'tre', 4: 'four', 5: 'five', 6: 'secs', 7: 'seven', 8: 'eith', 9: 'night', 10: 'ten'}
 
-def num_translate(a = 1):
+
+def num_translate(a):
     magick = numbers.get(a)
-    print(magick)
     return magick
 
-print(num_translate())
+
+print(num_translate(11))
