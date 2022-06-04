ce1e863a89511b940ad506a3ff38d7c6de1d2515
Alex86 <1a2v3a@gmail.com>
2022-06-04 20:31:35 +0500

    3 задание готово

--- Lesson04/03.py
+++ Lesson04/03.py
@@ -2,10 +2,10 @@
 # которая передаётся в ответе сервера. Дата должна быть в виде объекта date. 
 # Подумайте, как извлечь дату из ответа, 
 # какой тип данных лучше использовать в ответе функции?
-
+from datetime import datetime
 import requests
 import json
-import pprint
+import datetime as dt
 
 def currency_rates(currency):
     try:
@@ -14,9 +14,10 @@ def currency_rates(currency):
         dict_cbr = json.loads(req.text)
         msg = currency.upper()
         exchange_rates = dict_cbr['Valute'][msg]['Value']
-        # name_valute = dict_cbr['Valute'][msg]['Name'] # Если бь не условия задачи о том что должно вернутся 
-        # return f"{name_valute} {exchange_rates}"      # числовое значение то так мне больше нравится
-        return exchange_rates
+        name_valute = dict_cbr['Valute'][msg]['Name']
+        date_get = (dict_cbr['Date'][0:-15])
+        date_tru = dt.datetime.strptime(date_get, '%Y-%m-%d').date() # зачем ?
+        return f"Курс {name_valute} {exchange_rates} руб. на {date_tru}"
     except KeyError:
         return None
 
@@ -24,14 +25,3 @@ def currency_rates(currency):
 print(currency_rates("uSd"))
 print(currency_rates("euR"))
 print(currency_rates("111111111111"))
-
-
-# date_get = dict_cbr['Date']
-# name_valute = dict_cbr['Valute']['USD']['Name']
-
-# print(f"Курс {name_valute} на {date_get} составляет {exchange_rates} руб. ")
-
-#     print()
-#     return 0
-
-# #pprint.pprint(req.text)
