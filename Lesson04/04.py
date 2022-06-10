28c13af3c11b8326f723a5c2d2e2b3ec63e731aa
Alex86 <1a2v3a@gmail.com>
2022-06-10 13:41:30 +0500

    start

--- Lesson04/04.py
+++
@@ -1,7 +0,0 @@
-import utils
-
-print(utils.currency_rates("usd"))
-
-print(utils.currency_rates("EUr"))
-
-print(utils.currency_rates("111111"))
--- Lesson05/02.py
+++
@@ -1,5 +0,0 @@
-def gen_nums_no_yield(num):
-    return (num for num in range(1, num + 1) if num % 2 == 1)
-
-
-print(*gen_nums_no_yield(155))
