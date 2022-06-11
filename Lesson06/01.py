ef47993097672aedecbfe938ff2c23118a7fd3ca
Alex86 <1a2v3a@gmail.com>
2022-06-11 21:55:22 +0500

    dz

--- Lesson06/01.py
+++ Lesson06/01.py
@@ -1,1 +1,7 @@
-00
+with open('nginx_logs.txt', 'r', encoding='utf-8') as f:
+    for i in f.readlines():
+        ip = (i.split('-')[0])
+        request_type = (i.split('"')[1][0:3])
+        request_url = (i.split('"')[1][4:24])
+        request_tuple = (ip, request_type, request_url)
+        print(request_tuple)
