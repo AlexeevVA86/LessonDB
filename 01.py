d067a53b606d8609bc9b00d9b14b0a975b0e3d1f
alex86 <1a2v3a@gmail.com>
2022-06-20 20:42:35 +0500

    dzz

--- 01.py
+++ 01.py
@@ -1,23 +1,14 @@
-# 1. Написать скрипт, создающий стартер (заготовку) для проекта со следующей структурой папок:
-# |--my_project
-#    |--settings
-#    |--mainapp
-#    |--adminapp
-#    |--authapp
-# Примечание: подумайте о ситуации, когда некоторые папки уже есть на диске
-# (как быть?); как лучше хранить конфигурацию этого стартера, чтобы в
-# будущем можно было менять имена папок под конкретный проект;
-# можно ли будет при этом расширять конфигурацию и хранить данные
-# о вложенных папках и файлах (добавлять детали)?
+import re
+from functools import wraps
 
 
-import os
+def email_parse(email_address: str):
+    email_parser = re.compile('^(.+)@(.+\.[a-z]{2,})')
+    if not email_parser.match(email_address):
+        raise ValueError
 
-vood = {'my_project': ['settings', 'mainapp', 'adminapp', 'authapp']}
-for i, folders in vood.items():
-    if os.path.exists(i):
-        None
-    else:
-        for folder in folders:
-            cur_dir = os.path.join(i, folder)
-            os.makedirs(cur_dir)
+    passed_email = email_parser.findall(email_address)
+    return {'username': passed_email[0][0], 'domain': passed_email[0][1]}
+
+
+print(email_parse('someone@geekbrains.ru'))
--- 04.py
+++ 04.py
@@ -1,30 +1,20 @@
-# 4. Написать скрипт, который выводит статистику для заданной папки в виде словаря,
-# в котором ключи — верхняя граница размера файла (пусть будет кратна 10),
-# а значения — общее количество файлов (в том числе и в подпапках), размер которых не превышает этой границы,
-# но больше предыдущей (начинаем с 0), например:
-#
-#     {
-#       100: 15,
-#       1000: 3,
-#       10000: 7,
-#       100000: 2
-#     }
-# Тут 15 файлов размером не более 100 байт; 3 файла больше 100 и не больше 1000 байт...
-#
-# Подсказка: размер файла можно получить из атрибута .st_size объекта os.stat.
+def val_checker(predicate):
+    def _val_checker(func):
+        @wraps(func)  ## не совсем понял зачем мне здесь это
+        def wrapper(*args, **kwargs):
+            for arg in args + tuple(kwargs.values()):
+                if not predicate(arg):
+                    raise ValueError
+            return func(*args, **kwargs)
 
-import os
+        return wrapper
 
-root_dir = 'my_project'
-sizes_list = []
-for root, dirs, files in os.walk(root_dir):
-    for file in files:
-        file_size = os.stat(f'{root}/{file}').st_size
-        sizes_list.append(file_size)
-stat_dict = {}
-for i in range(1, len(str(max(sizes_list))) + 1):
-    listz = filter(lambda number: number <= 10 ** i and number > 10 ** (i - 1), sizes_list)
-    range_list = list(listz)
-    stat_dict[10 ** i] = len(range_list)
+    return _val_checker
 
-print(stat_dict)
+
+@val_checker(lambda x: x > 0)
+def calc_cube2(x):
+    return x ** 3
+
+
+print(calc_cube2(2))
